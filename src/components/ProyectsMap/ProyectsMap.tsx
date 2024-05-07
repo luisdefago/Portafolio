c5102094e821f865ab401a01bf8c7ede0b2bc9d0
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-05-06 22:36:53 -0300

    titulo proyects

--- src/components/ProyectsMap/ProyectsMap.tsx
+++ src/components/ProyectsMap/ProyectsMap.tsx
@@ -1,15 +1,17 @@
-// Projects.tsx
 import projectsData from '../../proyects.json'; 
 import Project from '../Proyect/Proyect';
 import { ProjectProps } from "../../utils/interface";
+
 const Projects = () => {
   return (
-    <div className="projects">
+    <section className="projects mt-16">
+      <h3 className="text-center text-4xl font-bold text-blue-950 mb-8">Mis Proyectos</h3> {/* Agrega márgenes superior e inferior y aumenta el tamaño del texto */}
       {projectsData.map((project: ProjectProps, index: number) => (
         <Project key={index} project={project} />
       ))}
-    </div>
+    </section>
   );
 };
 
 export default Projects;
+
