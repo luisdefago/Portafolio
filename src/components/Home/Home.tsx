063568bcbe12aaed3888fecfe3d3640cd3118a52
Luis Defago <102487252+luisdefago@users.noreply.github.com>
2024-05-07 14:08:16 -0300

    imagenes en proyectos

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return(
-    <div className="flex justify-center mt-24">
+    <div className="flex justify-center mt-10">
       <h2 className="text-6xl font-semibold text-blue-950">FullStack Developer</h2>
     </div>
   )
--- src/components/ProyectsMap/ProyectsMap.tsx
+++ src/components/ProyectsMap/ProyectsMap.tsx
@@ -7,7 +7,7 @@ const Projects = () => {
     <section className="mt-16">
       <h3 className="text-center text-4xl font-bold text-blue-950 mb-8">Mis Proyectos</h3>
       <div className="mx-auto max-w-7xl">
-        <div className="grid gap-8 lg:grid-cols-2">
+        <div className="grid gap-8 xl:grid-cols-2">
           {projectsData.map((project: ProjectProps, index: number) => (
             <Project key={index} project={project} />
           ))}
